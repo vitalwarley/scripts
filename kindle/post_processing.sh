8e41e4f4270ae008502369c96526b35b432fbb45
vitalwarley <vitalwarley@gmail.com>
2020-05-29 12:32:35 -0300

    Kindle: refactors folder

--- kindle/post_processing.sh
+++ kindle/post_processing.sh
@@ -3,3 +3,5 @@
 # remove header
 # remove first column (regex like '^\.,', anything from start to first comma)
 # add a "* " at the begin of each line (inserte in the regex, maybe with sed)
+# remove some: u0022
+# how to remove similar lines here? or I need python? pandas can do it?

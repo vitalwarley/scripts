880cb78dd032e4f69c438bb691423ee76c404d4d
vitalwarley <vitalwarley@gmail.com>
2020-05-30 12:09:32 -0300

    Hypothesis: full refactor
    - Added ArgumentParser
    - Parsed `date` option
    - Updated annotations instances to adequately sort them
        - `start` = 0 for page notes
        - For reply notes I assigned to `start` the end of its parent.
    - Copy notes to the clipboard

--- hypothesis/extract.py
+++ hypothesis/extract.py
@@ -2,57 +2,115 @@ import os
 import argparse
 import re
 from datetime import datetime
-from Hypothesis import Hypothesis as H
-from Hypothesis import HypothesisAnnotation
+
+import pyperclip
 from dotenv import load_dotenv
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--group-id')
-parser.add_argument('--date')
-parser.add_argument('--today', action='store_true')
+from Hypothesis import Hypothesis as H
+from Hypothesis import HypothesisAnnotation
 
-load_dotenv()
-USERNAME = os.getenv('USERNAME')
-TOKEN = os.getenv('TOKEN')
-
-h = H(username=USERNAME, token=TOKEN, debug=True)
-
-args = parser.parse_args()
-groupid = args.group_id
-date = args.date
-today = args.today
-
-if groupid is not None:
-    if today:
-        date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0).astimezone().isoformat()
-    elif date is not None:
-        # TODO: validate
-        pass
-    annotations = h.search_all(params={'search_after': date, 'order': 'asc', 'group': groupid})
+h = None
 
 
-#annotations = [{
-#    'id': a['id'],
-#    'created': a['created'],
-#    'group': a['group'],
-#    'document': a['document']['title'][0],
-#    'text': a['target'][0]['selector'][0]['selector'][1]['exact']
-#    } for a in annotations]
-annotations = [HypothesisAnnotation(row) for row in annotations]
-annotations = sorted(annotations, key=lambda x: x.start)
-
-def clean_text(text):
-    # spaces
-    text = re.sub(' +', ' ', text.strip())
-    # punctuation?
-    return text
+def parse_input():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--group-id')
+    parser.add_argument('--date')
+    parser.add_argument('--today', action='store_true')
+    return parser.parse_args()
 
 
+def connect_to_hypothesis():
+    load_dotenv()
+    USERNAME = os.getenv('USERNAME')
+    TOKEN = os.getenv('TOKEN')
+    h = H(username=USERNAME, token=TOKEN)
+    return h
+
+
+def debug():
+    # DEBUG
+    today = False
+    groupid = 'wdkVg8VA'
+    date = '29-05-2020'
+
+
+def extract(args=None):
+    if args is None:
+        return ''
+
+    groupid = args.group_id
+    date = args.date
+    today = args.today
+
+    if groupid is not None:
+        if today:
+            date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0).astimezone().isoformat()
+        elif date is not None:
+            d, m, y = map(int, date.split('-'))
+            date = datetime(y, m, d).astimezone().isoformat()
+        annotations_json = h.search_all(params={'search_after': date, 'order': 'asc', 'group': groupid})
+
+    annotations = [HypothesisAnnotation(row) for row in annotations_json]
+    return annotations
+
+
+def add_start_position_to_note(annotation):
+    """Adds parent start position to notes.
+
+    Annotations have a `type`: 'annotation', 'reply', or 'pagenote'.
+
+    Only the 'annotation' type have a `start` and `end`.
+
+    For sorting purposes, this function adds a `start` value for the 'reply' type
+    annotation. This value is the `end` value of its `references` (the first one).
+
+    Page notes annotations doesn't need to be sorted, as they are generic and not local-specific.
+    """
+    if annotation.type == 'reply':
+        parent_annotation = HypothesisAnnotation(h.get_annotation(annotation.references[0]))  # Why references is a list?
+        # And if parent_annotation is another reply? This will only work if the first reply has its start.
+        annotation.start = parent_annotation.end
+        annotation.text += ' #n'  # To categorize as a note.
+    elif annotation.is_page_note:
+        annotation.start = 0
+        annotation.text += ' #n'  # To categorize as a note.
+
+
+def preprocess(annotations):
+
+    def clean(text):
+        # spaces
+        text = re.sub(' +', ' ', text.strip())
+        # punctuation?
+        return text
+
+    for annotation in annotations:
+        add_start_position_to_note(annotation)
+        if annotation.type == 'annotation':
+            annotation.exact = clean(annotation.exact)
+            
+    annotations = sorted(annotations, key=lambda a: a.start)
+
+
+# TODO: how to extract references like (Author et al. 2020)?
 def main():
-    for a in annotations:
-        print(a.start, clean_text(a.exact))
-        print()
+    global h
+    h = connect_to_hypothesis()
 
+    args = parse_input()
+    annotations = extract(args)
+    preprocess(annotations)
+
+    output = ''
+    for a in annotations:
+        if a.type == 'reply' or a.is_page_note:
+            output += f' - {a.text}\n'
+        else:
+            output += f'- {a.exact}\n'
+
+    pyperclip.copy(output)
+    print('Done! Notes in the clipboard.')
 
 if __name__ == '__main__':
     main()

2b6d22df3719b04f361d8ebc78dcddaf956b7ab3
vitalwarley <vitalwarley@gmail.com>
2020-07-22 11:43:06 -0300

    kindle(md): adds levels

--- kindle/notes_to_md.py
+++ kindle/notes_to_md.py
@@ -11,13 +11,20 @@ notes = pd.read_csv(
     skiprows=[1, 2, 3, 4, 5, 6, 7],
 )
 
-notes['type'] = notes.type.str.split(r'(Nota|Amarelo|Azul)', expand=True)[1]
+notes['type'] = notes.type.str.split(r'(Nota|Amarelo|Azul|Rosa)', expand=True)[1]
 
 output = ''
+previous = None
+tabs = {'Azul': 2, 'Amarelo': 3, 'Rosa': 4}
 for idx, row in notes.iterrows():
     if row.type == 'Azul':
         output += f'\t- {row.annotation}\n'
-    else:
+    elif row.type == 'Amarelo':
         output += f'\t\t- {row.annotation}\n'
+    elif row.type == 'Rosa':
+        output += f'\t\t\t- {row.annotation}\n'
+    elif row.type == 'Nota':
+        output += "{}- {}\n".format(tabs[previous] * '\t', row.annotation)
+    previous = row.type
 
 pyperclip.copy(output)

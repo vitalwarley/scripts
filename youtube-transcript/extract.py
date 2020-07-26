56839d4b0a1c0ac1f1ef405d1c75d419cb9df95d
vitalwarley <vitalwarley@gmail.com>
2020-07-26 15:16:04 -0300

    YT: copy to clipboard

--- youtube-transcript/extract.py
+++ youtube-transcript/extract.py
@@ -1,5 +1,6 @@
 import argparse
 
+import pyperclip
 from punctuator import Punctuator
 from youtube_transcript_api import YouTubeTranscriptApi
 
@@ -8,6 +9,7 @@ parser.add_argument('--video-id')
 args = parser.parse_args()
 video_id = args.video_id
 transcript = YouTubeTranscriptApi.get_transcript(video_id)
+# TODO: parse to markdown
 text = ' '.join([t['text'] for t in transcript])
 punctutator = Punctuator('.punctuator/Demo-Europarl-EN.pcl')
-print(punctutator.punctuate(text))
+pyperclip.copy(punctutator.punctuate(text))

ac5c4c2f1ef645b6b56f09332f5d36dd108548b3
Warley Vital <vitalwarley@gmail.com>
2020-12-29 16:55:18 -0300

    adds option to save transcript to path

--- youtube-transcript/extract.py
+++ youtube-transcript/extract.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 
 import pyperclip
@@ -6,10 +7,21 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--video-id')
+parser.add_argument('--save-to', default=None)
 args = parser.parse_args()
+
 video_id = args.video_id
+save_to = args.save_to
+
 transcript = YouTubeTranscriptApi.get_transcript(video_id)
 # TODO: parse to markdown
 text = ' '.join([t['text'] for t in transcript])
 punctutator = Punctuator('.punctuator/Demo-Europarl-EN.pcl')
-pyperclip.copy(punctutator.punctuate(text))
+output = punctutator.punctuate(text)
+
+if save_to:
+    save_to = os.path.expanduser(save_to)
+    with open(save_to, 'w') as f:
+        f.write(output)
+else:
+    pyperclip.copy(output)
